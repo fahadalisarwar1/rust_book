c9f2e8405e52dc6b8ccfc80afbf2fbdd4383ea90
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2022-04-10 15:45:08 +0200

    Update main.rs

--- chapter_04/e_02_references/src/main.rs
+++ chapter_04/e_02_references/src/main.rs
@@ -32,7 +32,36 @@ fn main() {
         let r2 = &mut str1;
         println!("{}", r1);
         println!("{}", r2);  // this is an error
-        
-*/
 
+
+*/
+    
+    {
+        // this is ok becasue it is not in the same scope
+        let r2 = &mut str1;
+        println!("{}", r2);
+    }
+    let r1 = &mut str1;
+    println!("{}", r1);  
+
+    // you can create as many immuttable references as you want
+    // however you cant have mutable and immutable reference in the same scope
+
+    // in rust there are no dangling points
+
+    // summary
+    /* 
+    1: ownership is moved only for data types stored oon heap.
+    2: data types stored on stack do deep copy
+    3: tuples can be stored on stack or heap depending on their contents.
+    4: if the tuple contenets are primitive, they are stored on stack
+    5: references are used to borrow values
+    6: there can be many immutable references
+    7: there can be only only 1 mutable reference at a time
+    8: there cant be mutable and immutable reference in the same scope. 
+    9: you can use .clone() method to do deep copy explicitly
+    10: only mutable references can change the value of a variable
+    11: there are no dangling pointers 
+    
+    */
 }
