6f15134d5713046ecf2475c5d59a8041009b8fac
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2022-04-11 15:38:49 +0200

    Update main.rs

--- chapter_05/e_03_method_syntax/src/main.rs
+++ chapter_05/e_03_method_syntax/src/main.rs
@@ -1,3 +1,21 @@
+
+struct Rectangle{
+    width: i32, 
+    height: i32,
+}
+
+impl Rectangle {
+    // methods must have a parameter called &self. 
+    fn calculate_area(self: &Self)-> i32{ // this take a immutable borrow of self, it could also be a mutable borrow. 
+        self.width * self.height
+    }
+}
 fn main() {
     println!("Hello, world!");
+
+    // methods are similar to functions hwoever they are implemented on a struct. 
+    let rect = Rectangle{width:10, height:20};
+
+    println!("area = {}", rect.calculate_area());
+
 }
