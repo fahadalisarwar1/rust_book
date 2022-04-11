4a0cfc18a122c5122724a39db7907e955ae0f774
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2022-04-11 09:04:25 +0200

    Update main.rs

--- chapter_05/e_01_structs/src/main.rs
+++ chapter_05/e_01_structs/src/main.rs
@@ -1,3 +1,22 @@
+
+#[derive(Debug)]
+struct User{
+    name: String,
+    email: String,
+    login_count: u32,
+    is_active: bool,
+}
 fn main() {
-    println!("Hello, world!");
+    // structs are named compound data types that
+    let u1: User = User{
+        name: String::from("Fahad"),
+        email: String::from("fahad@gmail.com"),
+        login_count: 3,
+        is_active: true,
+    };
+
+    println!("the user name  is ={}", u1.name);
+    println!("the email is ={}", u1.email);
+    println!("the login_count is {}", u1.login_count);
+    println!("user active = {}", u1.is_active);
 }
